53df58b8401f7182ea423fcd67fc9eaffb53723a
Alejandro Astorga <alejandroadam34@hotmail.com>
2013-06-04 12:03:53 -0600

    Cambios temporales

--- administrador/views.py
+++ administrador/views.py
@@ -10,36 +10,6 @@ from django.contrib import admin
 from django.forms import *
 from administrador.models import *
 
-def vista1(request):
-	# import os
-	# RUTA_PROYECTO = os.path.dirname(os.path.realpath(__file__))
-	# print RUTA_PROYECTO
-	# user=User.objects.get(username='root')
-	# obj = UserForm(initial={'username':user.username,'password':user.password,'first_name':user.first_name,'last_name':user.last_name,'is_staff':user.is_staff,'is_superuser':user.is_superuser})
-	obj = UserForm(initial={'password':'aaaa',})
-	# print type(user.user_permissions.all())
-	# print dir(user.user_permissions.all())
-	# print user.user_permissions.all()
-	# print type(admin.site.login_form)
-	# print admin.site.login_form
-	# print dir(admin.site.login_form)
-	return render_to_response('pagina1.html',{'form':obj})
-
-@csrf_exempt
-def vista_ajax1(request):
-	if request.is_ajax():
-		print(request.POST)
-		if "form" in request.POST:
-			if "tipo" in request.POST:
-				return HttpResponse("true2")
-			else:
-				return HttpResponse("false2")	
-		else:
-			return HttpResponse("false1")
-	else:
-		return HttpResponse("No datos para ti.")
-
-
 def acerca_de(request):
 	return render_to_response('admin/acerca_de.html')
 
